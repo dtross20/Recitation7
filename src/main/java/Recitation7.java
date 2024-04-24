5e58edcce89a1f964521ca6dd1bf3f0338a41d0a
deyvon <dtross20@students.desu.edu>
2024-04-23 21:11:30 -0400

    Completed

--- src/main/java/Recitation7.java
+++ src/main/java/Recitation7.java
@@ -3,11 +3,27 @@ public class Recitation7 {
      * You may add any private helper method you see necessary,
      * but you may NOT change the signatures of the methods below.
      */
-    public static boolean lastItemReachable(int[] hopList){
-        return false; // replace this line with your code
+    public static boolean lastItemReachable(int[] hopList) {
+        int index = 0;
+        while (index < hopList.length - 1 && hopList[index] != 0) {
+            index += hopList[index];
+
+        }
+        return index >= hopList.length - 1;
     }
 
-    public static int[] slidingWindowMin(int[] numberList, int windowSize){
-        return new int[0]; // replace this line with your code
+    public static int[] slidingWindowMin(int[] numberList, int windowSize) {
+        int[] x1 = new int[numberList.length - windowSize + 1];
+        for (int x2 = 0; x2 < x1.length; x2++) {
+            int x3 = numberList[x2];
+            for (int x4 = x2; x4 < x2 + windowSize; x4++) {
+                if (x3 > numberList[x4]) {
+                    x3 = numberList[x4];
+                }
+            }
+            x1[x2] = x3;
+        }
+        return x1; // replace this line with your code
     }
 }
+
